0707b276f4d0c2a840822114873224de91c112f9
patel <parthpatel167@gmail.com>
2021-01-30 16:19:21 -0600

    cnages in app file

--- user_directory/src/App.js
+++ user_directory/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from 'react';
+import Container from './components/Container.js';
+import Tables from './components/Table.js';
+import Jumbotrons from './components/Jumbotrons.js';
+import './index.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid={true}>
+      <div className='App'>
+        <Jumbotrons />
+        <Tables  dark={true} />
+      </div>
+    </Container>
   );
 }
 
